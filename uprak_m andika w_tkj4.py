a9b9047da43cb9c0e166d063d56d2bff4245b2ca
angsclash <112526057+angsclash@users.noreply.github.com>
2022-09-09 14:19:39 +0700

    Add files via upload

---
+++ uprak_m andika w_tkj4.py
@@ -0,0 +1,34 @@
+# balok panjang * lebar * tinggi
+
+panjang = 100
+lebar = 5
+tinggi = 7
+balok = panjang * lebar * tinggi
+print("volume dari balok adalah", balok)
+
+# kubus sisi * sisi * sisi
+
+sisi = 7
+lkubus = sisi * sisi * sisi
+print("volume dari kubus adalah", lkubus)
+
+# limas 1/3 * luas_alas * tinggi
+
+luas_alas = 90
+tinggi = 10
+lulimas = 1/3 * luas_alas * tinggi
+print("volume dari limas adalah", lulimas)
+
+# tabung 22/7 * r * r * t
+
+r = 10
+t = 7
+ltabung = 22/7 * r * r * t
+print("volume dari tabung adalah", ltabung)
+
+# celcius to reamur = 5/4
+
+celcius = 1000
+reamur = 0.8
+rreamur = celcius * reamur
+print("celcius ke reamur", rreamur)
